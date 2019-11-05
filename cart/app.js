6f944795442069209ae8f8e9c82839534a8247c6
EduPassero <41768835+EduPassero@users.noreply.github.com>
2019-11-04 21:41:58 -0300

    Add files via upload

---
+++ cart/app.js
@@ -0,0 +1,42 @@
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartContent = document.querySelector('.cart-total');
+const productsDOM = document.querySelector('.products-center');
+// cart
+let cart = [];
+
+// getting the products
+class Products{
+async getProducts(){
+    try{
+        let result = await fetch('products.json')
+        let data = await result.json();
+        return data;
+    } catch (error){
+        console.log(error);
+    }
+    
+}
+}
+
+// display products
+class UI {
+
+}
+
+//local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded",() =>{
+    const ui = new UI();
+    const products = new Products();
+
+    // get all products
+    products.getProducts().then(data => console.log(data))
+})
